ee64d15b0c853cf0afff560e8cd1fdd99a017d16
Crumplee <raba.bence95@gmail.com>
2018-04-11 23:28:58 +0200

    behozando termek regisztralasa

--- SocketClient/BehozandoTermekRegisztralas.cs
+++ SocketClient/BehozandoTermekRegisztralas.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using AsynchronousClient;
 using Communication;
+using System.Collections.Generic;
 
 class BehozandoTermekRegisztralas
 {
@@ -33,17 +34,48 @@ class BehozandoTermekRegisztralas
             szabadRaklaphelyekListazasa(false);
         else szabadRaklaphelyekListazasa(true);
 
+        List<string> raklaphelyek = new List<string>();
+        for (int i = 0; i < mennyiseg; ++i)
+        {
+            Console.Write(i + ". raklap: ");
+            raklaphelyek.Add(Console.ReadLine());
+        }
+        Console.Write("Megerosit (i/h): ");
+        string valasz = Console.ReadLine();
+        if (valasz == "i")
+            adatokRogzitese(megrendeloAzonosito, termekNev, kulsoVonalkod, tipus, beIdopont, kiIdopont, mennyiseg, raklaphelyek);
     }
 
     public void szabadRaklaphelyekListazasa(bool hutott)
     {
         CommObject commObject = new CommObject("szabadRaklaphelyekListazasa");
         commObject.setHutott(hutott);
-        Console.Write("asdasdasdasdasdasdasd");
 
         Task<CommObject> tsResponse = SocketClient.SendRequest(commObject);
         Console.WriteLine("Sent request, waiting for response");
         CommObject dResponse = tsResponse.Result;
-        Console.WriteLine("Received response: " + dResponse);
+        foreach (string elem in dResponse.lista)
+        {
+            Console.WriteLine(elem);
+        }
+        
+    }
+
+    public void adatokRogzitese(string megrendeloAzonosito,
+                                string termekNev,
+                                string kulsoVonalkod,
+                                string tipus, 
+                                DateTime beIdopont, 
+                                DateTime kiIdopont, 
+                                int mennyiseg, 
+                                List<string> raklaphelyek)
+    {
+        CommObject commObject = new CommObject("behozandoTermekRogzitese");
+        commObject.termekAdatok = new CommObject.termekAdatokStruct(megrendeloAzonosito, termekNev, kulsoVonalkod, tipus, beIdopont, kiIdopont, mennyiseg, raklaphelyek);
+
+
+        Task<CommObject> tsResponse = SocketClient.SendRequest(commObject);
+        Console.WriteLine("Sent request, waiting for response");
+        CommObject dResponse = tsResponse.Result;
     }
 }
--- SocketClient/CommObject.cs
+++ SocketClient/CommObject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Communication
 {
@@ -7,16 +8,46 @@ namespace Communication
     {
         public string Message { get; set; }
         public bool hutott;
+        public List<string> lista;
+        public struct termekAdatokStruct
+        {
+            public string megrendeloAzonosito;
+            public string termekNev;
+            public string kulsoVonalkod;
+            public string tipus;
+            public DateTime beIdopont;
+            public DateTime kiIdopont;
+            public int mennyiseg;
+            public List<string> raklaphelyek;
 
+            public termekAdatokStruct(string _megrendeloAzonosito,
+                                string _termekNev,
+                                string _kulsoVonalkod,
+                                string _tipus,
+                                DateTime _beIdopont,
+                                DateTime _kiIdopont,
+                                int _mennyiseg,
+                                List<string> _raklaphelyek)
+            {
+                megrendeloAzonosito = _megrendeloAzonosito;
+                termekNev = _termekNev;
+                kulsoVonalkod = _kulsoVonalkod;
+                tipus = _tipus;
+                beIdopont = _beIdopont;
+                kiIdopont = _kiIdopont;
+                mennyiseg = _mennyiseg;
+                raklaphelyek = _raklaphelyek;
+            }
+        }
 
-
+        public termekAdatokStruct termekAdatok;
 
         public CommObject() { }
         public CommObject(string msg)
         {
             this.Message = msg;
         }
-        
+
         public void setHutott(bool value)
         {
             hutott = value;
